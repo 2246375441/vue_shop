a11aef909ea5cf0192bd4dc86b6ed7dc73f06f09
陈先生 <2246375441@qq.com>
2020-10-10 11:55:08 +0800

    vue_shop

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <!-- 路由占位符 2020/10/08 -->
-=======
     <!-- 路由占位符 2020/10/10  12:00 -->
->>>>>>> goods_params
     <router-view></router-view>
   </div>
 </template>
